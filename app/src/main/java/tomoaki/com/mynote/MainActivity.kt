902393735da0f6dddf466ac2ac72de46c7f215cf
SIRATEE-PC <siratee.tee@gmail.com>
2019-08-23 14:01:31 +0700

    connect DB to layout

--- app/src/main/java/tomoaki/com/mynote/MainActivity.kt
+++ app/src/main/java/tomoaki/com/mynote/MainActivity.kt
@@ -2,11 +2,32 @@ package tomoaki.com.mynote
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SimpleCursorAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DB
+        var objToCreateDB = MyNoteSQLiteOpenHelper(this)
+        var db = objToCreateDB.readableDatabase
+
+        var cursor = db.query("notes", arrayOf("_id","title"),
+            null, null, null, null, null)
+
+        //adapter
+        var listAdapter = SimpleCursorAdapter(this,
+            android.R.layout.simple_list_item_1,
+            cursor,
+            arrayOf("title"),
+            intArrayOf(android.R.id.text1),
+            0
+        )
+
+        //set adapter
+        listViewNote.adapter = listAdapter
     }
 }
--- app/src/main/java/tomoaki/com/mynote/MyNoteSQLiteOpenHelper.kt
+++ app/src/main/java/tomoaki/com/mynote/MyNoteSQLiteOpenHelper.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class MyNoteSQLiteOpenHelper(context :Context) :SQLiteOpenHelper(context , MynoteDB,null, 1) {
+class MyNoteSQLiteOpenHelper(context :Context) :SQLiteOpenHelper(context , "MynoteDB",null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
 
         db!!.execSQL("CREATE TABLE notes ("
