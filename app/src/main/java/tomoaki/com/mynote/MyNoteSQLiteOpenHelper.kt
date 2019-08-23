f76f65e3808bd398899e77dbae28fd28b139f861
SIRATEE-PC <siratee.tee@gmail.com>
2019-08-23 13:05:23 +0700

    DB oncreate finish

---
+++ app/src/main/java/tomoaki/com/mynote/MyNoteSQLiteOpenHelper.kt
@@ -0,0 +1,21 @@
+package tomoaki.com.mynote
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyNoteSQLiteOpenHelper(context :Context) :SQLiteOpenHelper(context , MynoteDB,null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        db!!.execSQL("CREATE TABLE notes ("
+                        + "_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + "title TEXT,"
+                        +"description TEXT)"
+
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
